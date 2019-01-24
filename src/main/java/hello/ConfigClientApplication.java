18961cf2fd237f97494a23437b6922ba878694ba
Tony.Li <Tony.Li@team.telstra.com>
2019-01-24 22:11:34 +1100

    Add logs to trigger building

--- src/main/java/hello/ConfigClientApplication.java
+++ src/main/java/hello/ConfigClientApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//Add logs to trigger buiding
 @SpringBootApplication
 public class ConfigClientApplication {
 
